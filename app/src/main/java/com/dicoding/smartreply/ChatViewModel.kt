246901ce91c9373ef305aadab594062ae846e12e
Nurdin <mrsunnysummer@gmail.com>
2024-11-11 01:31:16 +0700

    feat: create smart reply functionality using ml kit

--- app/src/main/java/com/dicoding/smartreply/ChatViewModel.kt
+++ app/src/main/java/com/dicoding/smartreply/ChatViewModel.kt
@@ -3,6 +3,14 @@ package com.dicoding.smartreply
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.mlkit.nl.smartreply.SmartReply
+import com.google.mlkit.nl.smartreply.SmartReplyGenerator
+import com.google.mlkit.nl.smartreply.SmartReplySuggestion
+import com.google.mlkit.nl.smartreply.SmartReplySuggestionResult
+import com.google.mlkit.nl.smartreply.TextMessage
+import java.lang.Exception
 
 class ChatViewModel : ViewModel() {
 
@@ -16,6 +24,8 @@ class ChatViewModel : ViewModel() {
 
     private val _errorMessage = MutableLiveData<String?>(null)
     val errorMessage: LiveData<String?> = _errorMessage
+
+    private val smartReply: SmartReplyGenerator = SmartReply.getClient()
 
     init {
         _pretendingAsAnotherUser.value = false
@@ -37,6 +47,48 @@ class ChatViewModel : ViewModel() {
         list.add(Message(message, !user, System.currentTimeMillis()))
 
         _chatHistory.value = list
+    }
+
+    private fun generateSmartReplyOptions(
+        messages: List<Message>,
+        isPretendingAsAnotherUser: Boolean
+    ): Task<List<SmartReplySuggestion>> {
+        val lastMessgae = messages.last()
+
+        if(lastMessgae.isLocalUser != isPretendingAsAnotherUser) {
+            return Tasks.forException(Exception("Tidak menjalankan smart replyy!"))
+        }
+
+        val chatConversations = ArrayList<TextMessage>()
+        for(message in messages) {
+            if(message.isLocalUser != isPretendingAsAnotherUser) {
+                chatConversations.add(TextMessage.createForLocalUser(message.text, message.timestamp))
+            } else {
+                chatConversations.add(
+                    TextMessage.createForRemoteUser(message.text, message.timestamp, anotherUserID)
+                )
+            }
+        }
+
+        return smartReply.suggestReplies(chatConversations)
+            .continueWith{ task ->
+                val result = task.result
+                when(result.status) {
+                    SmartReplySuggestionResult.STATUS_NOT_SUPPORTED_LANGUAGE ->
+                        _errorMessage.value = "Unable to generate options due to a non-English language was used"
+                    SmartReplySuggestionResult.STATUS_NO_REPLY ->
+                        _errorMessage.value = "Unable to generate options due to no appropriate response found"
+                }
+                result.suggestions
+            }
+            .addOnFailureListener{e ->
+                _errorMessage.value = "An error has occurred on Smart Reply Instance"
+            }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        smartReply.close()
     }
 
     companion object {
--- app/src/main/java/com/dicoding/smartreply/ReplyOptionsAdapter.kt
+++ app/src/main/java/com/dicoding/smartreply/ReplyOptionsAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.smartreply.databinding.ItemOptionsSmartreplyBinding
+import com.google.mlkit.nl.smartreply.SmartReplySuggestion
 
 class ReplyOptionsAdapter(
     private val onItemClickCallback: OnItemClickCallback
 ) : RecyclerView.Adapter<ReplyOptionsAdapter.ViewHolder>() {
+    private val smartReplyOptions = ArrayList<SmartReplySuggestion>()
 
     interface OnItemClickCallback {
         fun onOptionClicked(optionText: String)
@@ -27,11 +29,21 @@ class ReplyOptionsAdapter(
     }
 
     override fun onBindViewHolder(holder: ReplyOptionsAdapter.ViewHolder, position: Int) {
-
+        val optionText = smartReplyOptions[position].text
+        holder.binding.tvSmartReplyOption.text = optionText
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onOptionClicked(optionText)
+        }
     }
 
     override fun getItemCount(): Int {
-        return 0
+        return smartReplyOptions.size
+    }
+
+    fun setReplyOptions(smartReplyOptions: List<SmartReplySuggestion>) {
+        this.smartReplyOptions.clear()
+        this.smartReplyOptions.addAll(smartReplyOptions)
+        notifyDataSetChanged()
     }
 
 }
