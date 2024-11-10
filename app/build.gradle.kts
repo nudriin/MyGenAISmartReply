0183f68f7a2eba57a99eaceeed2f6559e2e62c15
Nurdin <mrsunnysummer@gmail.com>
2024-11-11 01:19:22 +0700

    chore: add ml kit smart reply dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,6 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("com.google.android.gms:play-services-mlkit-smart-reply:16.0.0-beta1")
 
 }
